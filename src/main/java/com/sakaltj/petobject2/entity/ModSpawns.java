3269de234185f2383b95918f6aca15079e9399af
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-05-02 21:06:29 +0900

    Update ModSpawns.java

--- src/main/java/com/sakaltj/petobject2/entity/ModSpawns.java
+++ src/main/java/com/sakaltj/petobject2/entity/ModSpawns.java
@@ -1,18 +1,34 @@
 package com.sakaltj.petobject2.entity;
 
-import com.sakalti.petobject2.EntityRegistry;
+import com.sakaltj.petobject2.EntityRegistry;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
-import net.minecraft.world.biome.BiomeKeys;
-import net.minecraft.world.biome.BiomeSelectors;
-import net.minecraft.world.SpawnSettings;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.SpawnSettings;
 
 public class ModSpawns {
+
     public static void register() {
+        // 属性登録
+        FabricDefaultAttributeRegistry.register(EntityRegistry.NORDIA, NordiaEntity.createAttributes());
+        FabricDefaultAttributeRegistry.register(EntityRegistry.COCLEA, CocleaEntity.createAttributes());
+
+        // Nordia: すべてのOVERWORLDのバイオームでスポーン
         BiomeModifications.addSpawn(
-            BiomeSelectors.all(),
+            BiomeSelectors.foundInOverworld(),
+            SpawnGroup.MONSTER,
+            EntityRegistry.NORDIA,
+            10, 1, 2
+        );
+
+        // Coclea: 平原と森に出現
+        BiomeModifications.addSpawn(
+            BiomeSelectors.categories(Biome.Category.FOREST, Biome.Category.PLAINS),
             SpawnGroup.CREATURE,
             EntityRegistry.COCLEA,
-            10, 1, 2
+            8, 1, 1
         );
     }
 }
