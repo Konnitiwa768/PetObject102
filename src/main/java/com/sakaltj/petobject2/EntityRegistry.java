0c969c16adcc3192aebc8bc9055b4286c4ad8e17
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-05-02 20:33:12 +0900

    Update EntityRegistry.java

--- src/main/java/com/sakaltj/petobject2/EntityRegistry.java
+++ src/main/java/com/sakaltj/petobject2/EntityRegistry.java
@@ -1,20 +1,19 @@
-package com.sakaltj.petobject2
+package com.sakalti.petobject2;
 
+import com.sakalti.petobject2.entity.CocleaEntity;
+import com.sakalti.petobject2.entity.NordnetiaEntity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
 
 public class EntityRegistry {
-    public static final EntityType<CocleaEntity> COCLEA = Registry.register(
-        Registry.ENTITY_TYPE,
-        new Identifier("petobject2", "coclea"),
-        EntityType.Builder.create(CocleaEntity::new, SpawnGroup.CREATURE)
-            .setDimensions(0.6f, 1.2f)
-            .build("coclea")
-    );
 
-    public static void registerAttributes() {
-        net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry.register(COCLEA, CocleaEntity.createCocleaAttributes());
+    public static final EntityType<NordnetiaEntity> NORDENTIA = register("nordnetia", EntityType.Builder.create(NordnetiaEntity::new, SpawnGroup.MONSTER).dimensions(EntityDimensions.fixed(16, 4)).build("nordnetia"));
+    public static final EntityType<CocleaEntity> COCLEA = register("coclea", EntityType.Builder.create(CocleaEntity::new, SpawnGroup.CREATURE).dimensions(EntityDimensions.fixed(16, 2)).build("coclea"));
+
+    private static <T extends Entity> EntityType<T> register(String name, EntityType<T> type) {
+        return Registry.register(Registry.ENTITY_TYPE, new Identifier("petobject2", name), type);
     }
 }
