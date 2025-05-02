9c960bfb74913271e0247badca6b71efe001f1d9
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-05-02 18:59:52 +0900

    Update EntityRegistry.java

--- src/main/java/com/sakaltj/petobject/EntityRegistry.java
+++ src/main/java/com/sakaltj/petobject/EntityRegistry.java
@@ -1,48 +1,19 @@
-package com.sakaltj.petobject;
-
-import com.sakalti.petobject.entity.LumeiEntity;
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
-import net.minecraft.entity.*;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
-
-public class EntityRegistry {
-    public static final EntityType<LumeiEntity> LUMEI = Registry.register(
-        Registry.ENTITY_TYPE,
-        new Identifier("petobject", "lumei"),
-        FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, LumeiEntity::new)
-            .dimensions(EntityDimensions.fixed(0.6f, 1.4f))
-            .build()
-    );
-
-    public stapackage com.sakalti.petobject.entity;
-
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
-import net.minecraft.entity.EntityDimensions;
+package:com.sakaltj.petobject
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class EntityRegistry {
-    public static final EntityType<LumeiEntity> LUMEI = Registry.register(
+    public static final EntityType<CocleaEntity> COCLEA = Registry.register(
         Registry.ENTITY_TYPE,
-        new Identifier("petobject", "lumei"),
-        FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, LumeiEntity::new)
-            .dimensions(EntityDimensions.fixed(0.6f, 1.2f)).build()
-    );
-
-    public static final EntityType<MechanylEntity> MECHANYL = Registry.register(
-        Registry.ENTITY_TYPE,
-        new Identifier("petobject", "mechanyl"),
-        FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, MechanylEntity::new)
-            .dimensions(EntityDimensions.fixed(0.8f, 1.95f)).build()
+        new Identifier("petobject2", "coclea"),
+        EntityType.Builder.create(CocleaEntity::new, SpawnGroup.CREATURE)
+            .setDimensions(0.6f, 1.2f)
+            .build("coclea")
     );
 
     public static void registerAttributes() {
-        net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry.register(
-            LUMEI, LumeiEntity.createLumeiAttributes());
-        net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry.register(
-            MECHANYL, MechanylEntity.createAttributes());
+        net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry.register(COCLEA, CocleaEntity.createCocleaAttributes());
     }
 }
