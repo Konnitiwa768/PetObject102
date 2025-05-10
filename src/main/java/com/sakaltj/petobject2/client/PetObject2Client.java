d09f59bf959466ee6669c3fd798b52fb27a67d8b
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-05-10 14:58:23 +0900

    Update PetObject2Client.java

--- src/main/java/com/sakaltj/petobject2/client/PetObject2Client.java
+++ src/main/java/com/sakaltj/petobject2/client/PetObject2Client.java
@@ -1,8 +1,8 @@
 package com.sakaltj.petobject2.client;
 
 import com.sakaltj.petobject2.EntityRegistry;
-import com.sakaltj.petobject2.client.renderer.CocleaRenderer;
-import com.sakaltj.petobject2.client.renderer.NordiaRenderer;
+import com.sakaltj.petobject2.client.renderer.*
+import com.sakaltj.petobject2.client.model.*
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.render.entity.EntityRenderers;
 
@@ -11,5 +11,9 @@ public class PetObject2Client implements ClientModInitializer {
     public void onInitializeClient() {
         EntityRenderers.register(EntityRegistry.NORDIA, NordiaRenderer::new);
         EntityRenderers.register(EntityRegistry.COCLEA, CocleaRenderer::new);
+        EntityRendererRegistry.register(EntityRegistry.MEMORIA, MemoriaRenderer::new);
+        EntityModelLayerRegistry.registerModelLayer(MEMORIA_LAYER, MemoriaModel::getTexturedModelData);
+        EntityModelLayerRegistry.registerModelLayer(NORDIA_LAYER, NordiaModel::getTexturedModelData);
+        EntityModelLayerRegistry.registerModelLayer(COCLEA_LAYER, CocleaModel::getTexturedModelData);
     }
 }
